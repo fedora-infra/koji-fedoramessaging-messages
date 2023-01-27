3de7c6dd5deefd639969e2047ac3db8ef4b795ef
Ryan Lerch <rlerch@redhat.com>
2023-01-27 14:27:43 +1000

    Refactor task schema to remove code duplication
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- koji_fedoramessaging_messages/tag.py
+++ koji_fedoramessaging_messages/tag.py
@@ -16,172 +16,102 @@
 
 from .base import KojiFedoraMessagingMessage, SCHEMA_URL
 
+TAG = {
+    "type": "object",
+    "properties": {
+        "build_id": {"type": "integer", "description": "build id"},
+        "name": {"type": "string", "description": "package name"},
+        "tag_id": {
+            "type": "integer",
+            "description": "tag id",
+        },
+        "instance": {
+            "type": "string",
+            "description": "distinguish between messages from primary and secondary koji",
+        },
+        "tag": {
+            "type": "string",
+            "description": "name of the tag",
+        },
+        "user": {
+            "type": "string",
+            "description": "name of the user that trigger the build",
+        },
+        "version": {
+            "type": "string",
+            "description": "version of the build",
+        },
+        "owner": {
+            "type": "string",
+            "description": "name of the package owner",
+        },
+        "release": {
+            "type": "string",
+            "description": "release number of the package",
+        },
+    },
+}
 
-class TagV1(KojiFedoraMessagingMessage):
+
+class TagMessage(KojiFedoraMessagingMessage):
+    @property
+    def build_id(self) -> int:
+        return self.body.get("build_id")
+
+    @property
+    def name(self) -> str:
+        return self.body.get("name")
+
+    @property
+    def tag_id(self) -> int:
+        return self.body.get("tag_id")
+
+    @property
+    def instance(self) -> str:
+        return self.body.get("instance")
+
+    @property
+    def tag(self) -> str:
+        return self.body.get("tag")
+
+    @property
+    def user(self) -> str:
+        return self.body.get("user")
+
+    @property
+    def version(self) -> str:
+        return self.body.get("version")
+
+    @property
+    def owner(self) -> str:
+        return self.body.get("owner")
+
+    @property
+    def release(self) -> str:
+        return self.body.get("release")
+
+
+class TagV1(TagMessage):
     """This message is sent when a package is tagged."""
 
     topic = "buildsys.tag"
-
     body_schema = {
         "$id": f"{SCHEMA_URL}/v1/{topic}#",
         "$schema": "https://json-schema.org/draft/2019-09/schema",
         "description": "A package is tagged.",
         "type": "object",
-        "properties": {
-            "build_id": {"type": "integer", "description": "build id"},
-            "name": {"type": "string", "description": "package name"},
-            "tag_id": {
-                "type": "integer",
-                "description": "tag id",
-            },
-            "instance": {
-                "type": "string",
-                "description": "distinguish between messages from primary and secondary koji",
-            },
-            "tag": {
-                "type": "string",
-                "description": "name of the tag",
-            },
-            "user": {
-                "type": "string",
-                "description": "name of the user that trigger the build",
-            },
-            "version": {
-                "type": "string",
-                "description": "version of the build",
-            },
-            "owner": {
-                "type": "string",
-                "description": "name of the package owner",
-            },
-            "release": {
-                "type": "string",
-                "description": "release number of the package",
-            },
-        },
+        "properties": TAG,
     }
 
-    @property
-    def build_id(self) -> int:
-        return self.body.get("build_id")
 
-    @property
-    def name(self) -> str:
-        return self.body.get("name")
-
-    @property
-    def tag_id(self) -> int:
-        return self.body.get("tag_id")
-
-    @property
-    def instance(self) -> str:
-        return self.body.get("instance")
-
-    @property
-    def tag(self) -> str:
-        return self.body.get("tag")
-
-    @property
-    def user(self) -> str:
-        return self.body.get("user")
-
-    @property
-    def version(self) -> str:
-        return self.body.get("version")
-
-    @property
-    def owner(self) -> str:
-        return self.body.get("owner")
-
-    @property
-    def release(self) -> str:
-        return self.body.get("release")
-
-
-class UntagV1(KojiFedoraMessagingMessage):
+class UntagV1(TagMessage):
     """This message is sent when a package is untagged."""
 
     topic = "buildsys.untag"
-
     body_schema = {
         "$id": f"{SCHEMA_URL}/v1/{topic}#",
         "$schema": "https://json-schema.org/draft/2019-09/schema",
         "description": "A package is untagged.",
         "type": "object",
-        "properties": {
-            "build_id": {"type": "integer", "description": "build id"},
-            "name": {"type": "string", "description": "package name"},
-            "tag_id": {
-                "type": "integer",
-                "description": "tag id",
-            },
-            "instance": {
-                "type": "string",
-                "description": "distinguish between messages from primary and secondary koji",
-            },
-            "tag": {
-                "type": "string",
-                "description": "name of the tag",
-            },
-            "user": {
-                "type": "string",
-                "description": "name of the user that trigger the build",
-            },
-            "version": {
-                "type": "string",
-                "description": "version of the build",
-            },
-            "owner": {
-                "type": "string",
-                "description": "name of the package owner",
-            },
-            "release": {
-                "type": "string",
-                "description": "release number of the package",
-            },
-        },
+        "properties": TAG,
     }
-
-    @property
-    def build_id(self) -> int:
-        return self.body.get("build_id")
-
-    @property
-    def name(self) -> str:
-        return self.body.get("name")
-
-    @property
-    def tag_id(self) -> int:
-        return self.body.get("tag_id")
-
-    @property
-    def instance(self) -> str:
-        return self.body.get("instance")
-
-    @property
-    def tag(self) -> str:
-        return self.body.get("tag")
-
-    @property
-    def user(self) -> str:
-        return self.body.get("user")
-
-    @property
-    def version(self) -> str:
-        return self.body.get("version")
-
-    @property
-    def owner(self) -> str:
-        return self.body.get("owner")
-
-    @property
-    def release(self) -> str:
-        return self.body.get("release")
-
-    @property
-    def agent_name(self):
-        return self.user
-
-    @property
-    def usernames(self):
-        return [self.agent_name, self.owner]
