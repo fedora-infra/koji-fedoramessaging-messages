f4d294695bbe1013bc93857418cedf19a0efbcb7
Aurélien Bompard <aurelien@bompard.org>
2023-04-03 12:51:38 +0200

    Add Packit integration
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ make-spec.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+LANG=C
+SPEC=python-koji-fedoramessaging-messages.spec
+VERSION=$(poetry version -s)
+AUTHOR=$(git log -1 --format="%an <%ae>")
+CHANGELOG="* `date +"%a %b %d %Y"` ${AUTHOR} - ${VERSION}-1\n- Version ${VERSION}"
+
+sed -e '
+    s/@@VERSION@@/'${VERSION}'/g;
+    $a\
+'"${CHANGELOG}"'
+    ' ${SPEC}.in > ${SPEC}
